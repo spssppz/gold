fbd89e11c0a90d44349e910fa7c43c9071768202
spssppz <115707565+spssppz@users.noreply.github.com>
2024-02-28 08:49:17 +0300

    Add files via upload

---
+++ content/js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+	$(window).scroll(function () { if ($(this).scrollTop() > 0) { $('#scroller').fadeIn(); } else { $('#scroller').fadeOut(); } });
+	$('#scroller').click(function () { $('body,html').animate({ scrollTop: 0 }, 400); return false; });
+});
